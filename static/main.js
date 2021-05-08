c10ffe086143269085b2216d60bf3497c24afd2a
Eirik Vale Aase <e.v.aase@gmail.com>
2021-05-09 00:14:48 +0200

    Remove footer for now

--- static/main.js
+++
@@ -1,9 +0,0 @@
-function setFooterText() {
-    const date = new Date().getFullYear();
-    const footerText = `<p>Eirik Vale Aase © ${date.toString()}</p>`
-
-    const footer = document.getElementById('footer');
-    footer.innerHTML = footerText;
-}
-
-setFooterText();
